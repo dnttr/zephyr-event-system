472bf389178d96443eafbad238e4c958d8943878
dnttr <147146363+dnttr@users.noreply.github.com>
2025-07-05 17:16:31 +0200

    Improve thread safety in EventBus by using CopyOnWriteArrayList for subscribers

--- src/main/java/org/dnttr/zephyr/event/EventBus.java
+++ src/main/java/org/dnttr/zephyr/event/EventBus.java
@@ -5,26 +5,30 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-/*
- * Scheduled for removal. This class is a temporary solution for the event bus.
- * It will be replaced with a more robust implementation in the future.
- */
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class EventBus {
 
-    private final List<EventMethod> subscribers = new ArrayList<>();
+    private final List<EventMethod> subscribers = new CopyOnWriteArrayList<>();
 
     public void register(Object clazz) {
         Class<?> cl = clazz.getClass();
-        Arrays.stream(cl.getDeclaredMethods()).filter(declaredMethod -> declaredMethod.isAnnotationPresent(EventSubscriber.class))
+        Arrays.stream(cl.getDeclaredMethods())
+                .filter(declaredMethod -> declaredMethod.isAnnotationPresent(EventSubscriber.class))
                 .filter(declaredMethod -> declaredMethod.getParameterCount() > 0)
-                .map(declaredMethod -> new EventMethod(declaredMethod, clazz))
+                .map(declaredMethod -> {
+                    declaredMethod.setAccessible(true);
+                    return new EventMethod(declaredMethod, clazz);
+                })
                 .forEach(subscribers::add);
     }
 
     public void unregister(Object clazz) {
-        this.subscribers.removeAll(this.subscribers.stream().filter(eventMethod -> eventMethod.clazz().equals(clazz)).toList());
+        List<EventMethod> toRemove = this.subscribers.stream()
+                .filter(eventMethod -> eventMethod.clazz().equals(clazz))
+                .toList();
+
+        this.subscribers.removeAll(toRemove);
     }
 
     public void call(Event event) {
